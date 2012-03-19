0d69e55d33c14dae18e912ec6e44cbb36ca9bfd6
Alex Chaffee <alex@stinky.com>
2012-03-19 08:43:30 -0700

    generated

--- 05_pig_latin/pig_latin_spec.rb
+++ 05_pig_latin/pig_latin_spec.rb
@@ -41,4 +41,8 @@ describe "#translate" do
     s.should == "ethay ickquay ownbray oxfay"
   end
 
+  # Test-driving bonus:
+  # * write a test asserting that capitalized words are still capitalized (but with a different initial capital letter, of course)
+  # * retain the punctuation from the original phrase
+
 end
