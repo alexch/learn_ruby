43b318ed0b38bc64e53bb3fa36e5dd52a6aa407e
mariozig <mariozig@gmail.com>
2012-12-19 20:58:15 -0800

    Updating text instructions to reflect the correct number of methods required in order to satisfy the tests

--- 02_calculator/calculator_spec.rb
+++ 02_calculator/calculator_spec.rb
@@ -25,7 +25,7 @@
 #
 # There are also some bonus exercises for when you finish the regular ones. The bonus round will help teach you test-driven *development*, not simply test-guided *learning*.
 #
-# Your mission, should you choose to accept it, is to write *tests* for two new methods:
+# Your mission, should you choose to accept it, is to write *tests* for three new methods:
 #
 # * `multiply` which multiplies two numbers together
 # * `power` which raises one number to the power of another number
