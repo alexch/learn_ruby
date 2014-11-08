92e176fd4d2317e7018bcdebc7f2884815052822
Coy Sanders <coymeetsworld@gmail.com>
2014-11-08 02:47:36 -0800

    Adding comment header to solution.

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,3 +1,7 @@
+# Solution to problem 01_temperature from learn_ruby
+# Author: Coy Sanders (coymeetsworld)
+# Date: 11/08/14
+
 def ftoc(fahrenheit_temperature)
 	return (fahrenheit_temperature - 32.0) * 5.0 / 9.0
 end
