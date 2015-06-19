e4f39787ff7744a3bce6f2c6eb6669e55de3a868
Erik Trautman <eriktrautman@users.noreply.github.com>
2015-06-18 17:36:01 -0700

    Update dictionary_spec.rb

--- 11_dictionary/dictionary_spec.rb
+++ 11_dictionary/dictionary_spec.rb
@@ -36,7 +36,7 @@ describe Dictionary do
   it "doesn't cheat when checking whether a given keyword exists" do
     @d.include?('fish').should be false # if the method is empty, this test passes with nil returned
     @d.add('fish')
-    @d.include?('fish').should be_true # confirms that it actually checks
+    @d.include?('fish').should be true # confirms that it actually checks
     @d.include?('bird').should be false # confirms not always returning true after add
   end
 
