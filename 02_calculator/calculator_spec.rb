3e15bd46c07fbe62e0f2a2b6afb08d4bcf09d84d
Alex Negru <alex.negru@agilefreaks.com>
2018-11-13 17:04:22 +0200

    Add tests for multiply, power, factorial functions

--- 02_calculator/calculator_spec.rb
+++ 02_calculator/calculator_spec.rb
@@ -79,21 +79,41 @@ end
 
 describe "#multiply" do
 
-  it "multiplies two numbers"
-
-  it "multiplies several numbers"
+  it "multiplies two numbers" do
+    multiply(10, 5).should == 50
+  end
+  
+  it "multiplies several numbers" do
+    multiply(5, 10, 15).should == 750
+  end
 
 end
 
 describe "#power" do
-  it "raises one number to the power of another number"
+  it "raises one number to the power of another number" do
+    power(2, 5).should == 32
+  end
 end
 
 # http://en.wikipedia.org/wiki/Factorial
 describe "#factorial" do
-  it "computes the factorial of 0"
-  it "computes the factorial of 1"
-  it "computes the factorial of 2"
-  it "computes the factorial of 5"
-  it "computes the factorial of 10"
+  it "computes the factorial of 0" do
+    factorial(0).should == 1
+  end
+
+  it "computes the factorial of 1" do
+    factorial(1).should == 1
+  end
+
+  it "computes the factorial of 2" do
+    factorial(2).should == 2
+  end
+
+  it "computes the factorial of 5" do
+    factorial(5).should == 120
+  end
+
+  it "computes the factorial of 10" do
+    factorial(10).should == 3628800
+  end
 end
