4000684b30778cbb9c5304b28697778682910e40
Alex Negru <alex.negru@agilefreaks.com>
2018-11-14 14:22:19 +0200

    Adjust indentation

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,24 +1,24 @@
 def add(a, b)
-    a + b
+	a + b
 end
 
 def subtract(a, b)
-    a - b
+  a - b
 end
 
 def sum(arr)
-    arr.inject(0, :+)
+  arr.inject(0, :+)
 end
 
 def multiply(*numbers)
-    numbers.inject(1, :*)
+  numbers.inject(1, :*)
 end
 
 def power(a, b)
-    a ** b
+  a ** b
 end
 
 def factorial(n)
-    return 1 if n == 0
-    n * factorial(n - 1)
+	return 1 if n == 0
+	n * factorial(n - 1)
 end
