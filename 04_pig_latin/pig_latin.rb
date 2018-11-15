56d4d6a5322b8a68025d54c09e6b1faf82fcf9b1
Alex Negru <alex.negru@agilefreaks.com>
2018-11-15 13:57:41 +0200

    Count 'qu' as a consonant

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,18 +1,24 @@
 def translate(s)
-    vowels = %w(a e i o u)
-    consonants = %(b c d f g h j k l m n p q r t v w x y z)
+	vowels = %w(a e i o u)
+	consonants = %(b c d f g h j k l m n p q r s t v w x y z)
 
 	s = s.split.map do |word|
-        if vowels.include?(word[0])
-            word.concat("ay")
-        end
-        
-        while consonants.include?(word[0]) 
-            word.concat(word[0])
-            word = word[1..-1]
-            word = word + "ay" if vowels.include?(word[0])
-        end
-        word
-    end
+		if vowels.include?(word[0])
+			word.concat("ay")
+		end
+		
+		while consonants.include?(word[0])
+			if word[0..1] == "qu"
+				word.concat("qu")
+				word = word[2..-1]
+				word = word + "ay"
+			else
+				word.concat(word[0])
+				word = word[1..-1]
+				word = word + "ay" if vowels.include?(word[0])
+			end
+		end
+		word
+	end
 	s.join(' ')
 end
