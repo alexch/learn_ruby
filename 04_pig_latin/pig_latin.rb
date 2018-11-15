c2aae23b53db5d01a5f60527a0abcdc62367438c
Alex Negru <alex.negru@agilefreaks.com>
2018-11-15 14:38:35 +0200

    Reduce duplicate code

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,10 +1,11 @@
 def translate(phrase)
 	vowels = /[aeiou]/
 	consonants = /[b-df-hj-np-tv-z]/
+	
+	doVowel = proc { |word| word.concat("ay") if vowels.match(word[0]) }
 
 	phrase = phrase.split.map do |word|
-		word.concat("ay") if vowels.match(word[0])
-		
+		doVowel.call(word)
 		while consonants.match(word[0])
 			if word[0..1] == "qu"
 				word.concat("qu")
@@ -13,7 +14,7 @@ def translate(phrase)
 			else
 				word.concat(word[0])
 				word = word[1..-1]
-				word.concat("ay") if vowels.match(word[0])
+				doVowel.call(word)
 			end
 		end
 		word
