27c693d0892b7773d3e8f5cbc93be9c701aabfed
Alex Negru <alex.negru@agilefreaks.com>
2018-11-19 09:57:49 +0200

    Improve variable naming

--- 06_performance_monitor/performance_monitor.rb
+++ 06_performance_monitor/performance_monitor.rb
@@ -1,12 +1,12 @@
 require 'time'
 
 def measure(amount = 1)
-  average_time = 0
+  total_time = 0
   amount.times do
     start_time = Time.now
     yield
     end_time = Time.now 
-    average_time += end_time - start_time
+    total_time += end_time - start_time
   end
-  average_time / amount
+  average_time = total_time / amount
 end
