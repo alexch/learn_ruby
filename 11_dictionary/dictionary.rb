c4e963185e0df74fab405d3143ab785b8888f13c
Alex Negru <alex.negru@agilefreaks.com>
2018-11-20 13:17:52 +0200

    Add entries with or without definiton

--- 11_dictionary/dictionary.rb
+++ 11_dictionary/dictionary.rb
@@ -2,4 +2,16 @@ class Dictionary
   def entries
     @entries ||= {}
   end
+
+  def add(hash)
+    if hash.is_a? Hash
+      entries.merge!(hash)
+    else # input is a String
+      entries[hash] = nil
+    end 
+  end
+
+  def keywords
+    entries.keys
+  end
 end
