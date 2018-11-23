c85b04584a38ed466eb26fd81fe5044ee7719dd3
Alex Negru <alex.negru@agilefreaks.com>
2018-11-23 13:17:16 +0200

    Add indentation to tags

--- 13_xml_document/xml_document.rb
+++ 13_xml_document/xml_document.rb
@@ -1,18 +1,32 @@
 class XmlDocument
+  def initialize(indented=false)
+    @xml = ""
+    @indented = indented
+    @level = 0
+  end
+
   def method_missing(method, attributes = {}, &block)
-    xml = ''
+    @xml =''
     tag = method.to_s
-    attributes_string = ''
+    attributes_string = ""
 
     attributes.each { |k, v| attributes_string += " #{k}='#{v}'" }
-    xml += "<#{tag + attributes_string}>"
+    @xml += "<#{tag + attributes_string}>"
 
     if block_given?
-      xml += "#{yield}</#{tag}>"
+      format if @indented
+      @xml += "#{yield}</#{tag}>"
+      format(true) if @indented
     else
-      xml.gsub!(/>/, "/>")
+      @xml.gsub!(/>$/, "/>")
+      format(true) if @indented
     end
+    @xml
+  end
 
-    xml
+  def format(end_tag=false)
+    @level += 1 unless end_tag
+    @level -= 1 if end_tag && @level > 0
+    @xml += "\n" + "  " * @level
   end
 end
